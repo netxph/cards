b32d71c6ba06a5de4fc0958766bc18bf7a4a3f23
Marc Vitalis <marc.vitalis@live.com>
2013-06-14 17:18:41 +0800

    add test framework

---
+++ src/Cards.Tests/TestCase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Tests
+{
+    public abstract class TestCase<T> : IDisposable
+    {
+
+        protected T Subject { get; set; }
+
+        public TestCase()
+        {
+            Initialize();
+        }
+
+        protected virtual void Initialize()
+        {
+            
+        }
+
+        protected abstract T Given();
+
+        protected virtual void Destroy()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            Destroy();    
+        }
+        
+    }
+}
