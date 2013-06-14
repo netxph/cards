4ee0f86dd43322ad1e238c4b9136aadc41d749e9
Marc Vitalis <marc.vitalis@live.com>
2013-06-15 06:22:12 +0800

    add under repair landing page

---
+++ src/Cards.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cards.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Repair()
+        {
+            return View();
+        }
+
+    }
+}
