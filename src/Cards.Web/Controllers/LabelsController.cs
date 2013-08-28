a0440ea8e6c00072deea1e46f6557b8667c320d4
Marc Vitalis <marc.vitalis@live.com>
2013-08-28 18:03:23 +0800

    add bare page for labels

---
+++ src/Cards.Web/Controllers/LabelsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cards.Web.Controllers
+{
+    public class LabelsController : Controller
+    {
+        //
+        // GET: /Labels/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
