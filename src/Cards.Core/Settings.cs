99fd6b12392bdc4adcdada07014ebbd1e6bdb17c
Marc Vitalis <marc.vitalis@live.com>
2013-09-01 19:28:45 +0800

    moved settings to single class

---
+++ src/Cards.Core/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Core
+{
+    public static class Settings
+    {
+        public const int NEW_AGE = 0;
+        public const int OLD_AGE = 30;
+        public const int STALE_AGE = 10;
+    }
+}
