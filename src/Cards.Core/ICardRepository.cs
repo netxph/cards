5e9c7a1bf4d97ef02203c7a0343bc7102c2fc3dc
mejas <lego_boy11@yahoo.com>
2013-09-06 16:12:00 +0800

    added CRUD operation tests to user (no db implementation)

--- src/Cards.Core/ICardRepository.cs
+++ src/Cards.Core/ICardRepository.cs
@@ -8,7 +8,6 @@ namespace Cards.Core
 {
     public interface ICardRepository
     {
-
         Area CreateArea(Area area);
         List<Area> FindAllArea();
         Area FindArea(int id);
@@ -24,5 +23,9 @@ namespace Cards.Core
         List<Label> FindAllLabels();
 
         Label DeleteLabel(Label label);
+
+        User CreateUser(User user);
+        User FindUser(int id);
+        User UpdateUser(User user);
     }
 }
