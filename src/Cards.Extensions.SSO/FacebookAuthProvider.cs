575a4f2b1fcc67ac366657e9360963352275172d
Marc Vitalis <marc.vitalis@live.com>
2013-10-06 22:02:14 +0800

    add facebook authentication

--- src/Cards.Extensions.SSO/FacebookAuthProvider.cs
+++ src/Cards.Extensions.SSO/FacebookAuthProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Cards.Core;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,26 @@ namespace Cards.Extensions.SSO
     public class FacebookAuthProvider : IAuthenticationProvider
     {
         const string PROVIDER_NAME = "FacebookSSO";
+        const string ACCESS_TOKEN_PARAM = "access_token";
+        const string USER_DATA_KEY = "email";
+        const string INFO_URL = "https://graph.facebook.com/me";
 
         public string Authenticate(IUserCredentials credentials)
         {
-            return "marc";
+            var cred = credentials as TokenCredentials;
+
+            var client = new RestClient(INFO_URL);
+            var request = new RestRequest(Method.GET);
+            request.AddParameter(ACCESS_TOKEN_PARAM, cred.Token);
+
+            var response = client.Execute<Dictionary<string, string>>(request);
+
+            if (response.Data != null && response.Data.ContainsKey(USER_DATA_KEY))
+            {
+                return response.Data[USER_DATA_KEY];
+            }
+
+            return null;
         }
 
         public string Name
