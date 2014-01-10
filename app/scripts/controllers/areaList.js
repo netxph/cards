a27b58765377901e229bacf26f26659a1c4eb36c
Marc Vitalis <marc.vitalis@live.com>
2014-01-10 18:46:23 +0800

    convert service to

--- app/scripts/controllers/areaList.js
+++ app/scripts/controllers/areaList.js
@@ -2,6 +2,10 @@
 
 var cardsApp = angular.module('cardsApp');
 
-cardsApp.controller('AreaListCtrl', ['$scope', 'areaService', function($scope, areaService) {
-    $scope.areas = areaService.getAreas(); 
+cardsApp.controller('AreaListCtrl', ['$scope', 'Areas', function($scope, Areas) {
+    $scope.data = {};
+    
+    Areas.query(function(response) {
+        $scope.data.areas = response; 
+    });
 }]);
--- app/scripts/services/areaService.js
+++ app/scripts/services/areaService.js
@@ -2,22 +2,6 @@
 
 var cardsApp = angular.module('cardsApp');
 
-cardsApp.factory('areaService', function() {
-    return {
-        getAreas: function() {
-
-            var areas = []; 
-
-            return [
-            {
-                name: "Backlog",
-                cards: [
-                {
-                    name: "Create a design template for cards",
-                    description: "description goes here",
-                    labels: ["Bug"]
-                }]
-            }];
-        },
-    };
+cardsApp.factory('Areas', function($resource) {
+    return $resource('http://localhost/areas', {});
 });
